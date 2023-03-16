2f8db3f0430fca7a891ccb4044c7fe50d8223cb0
Eric Olsen <126840543+eccol@users.noreply.github.com>
2023-03-16 19:32:52 -0400

    Add basic calculator functionality

--- calculator.js
+++ calculator.js
@@ -1,10 +1,41 @@
 let display = '';
+let a = 0;
+let op = '';
+let b = 0;
 
-const buttons = document.querySelectorAll('.digit, .operator');
+// When digit clicked, append to display
+const buttons = document.querySelectorAll('.digit');
 buttons.forEach((btn) => btn.addEventListener('click', () => {
   display += btn.id;
-  document.querySelector('.result').textContent = display;
 }))
+
+// When operator clicked, save previous number and operator
+operators = document.querySelectorAll('.operator');
+operators.forEach((btn) => btn.addEventListener('click', () => {
+  a = Number(display);
+  op = btn.id;
+  display = '';
+}))
+
+// When equals clicked, save the second number and run the function
+document.querySelector('.equals').addEventListener('click', () => {
+  b = Number(display);
+  result = operate(a, b, op);
+  display = result;
+})
+
+// When clear clicked, clear the display and empty the variables
+document.querySelector('.clear').addEventListener('click', () => {
+  display = '';
+  a = 0;
+  b = 0;
+  op = '';
+})
+
+// This has to stay at the end of the event listeners so it updates *after* the selection
+document.querySelectorAll('button').forEach((btn) => {
+  btn.addEventListener('click', () => { document.querySelector('.result').textContent = display })
+})
 
 function add(a, b) {
   return a + b;
